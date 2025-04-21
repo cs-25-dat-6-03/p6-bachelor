c2ae97744723e00fd95f318f0f485530074d5452
IO32NQ <113096079+IO32NQ@users.noreply.github.com>
2025-04-21 23:57:55 +0200

    .

--- main/algorithms/MF_ALS.py
+++ main/algorithms/MF_ALS.py
@@ -6,42 +6,25 @@ filepath = "dataset/"
 movies = pd.read_csv(filepath + "movies.csv")
 ratings = pd.read_csv(filepath + "ratings.csv")
 
-movie_ratings = ratings.merge(movies, on="movieId")
-ratings_df = movie_ratings[['userId', 'movieId', 'rating']]
+#Create User/Item matrix
+pivot_table = ratings.pivot(index='userId', columns='movieId', values='rating')
+pivot_table = pivot_table.fillna(0)
+rating_matrix = pivot_table.to_numpy()
 
-# Normalize ratings to a 0-1 scale
-min = ratings_df["rating"].min()
-max = ratings_df["rating"].max()
+# Normalize ratings
+min = ratings["rating"].min()
+max = ratings["rating"].max()
 
 # Determine the number of users and movies
-num_users = ratings_df['userId'].max()
-num_movies = ratings_df['movieId'].max()
-print(num_users, num_movies, "\n")
+num_users = ratings['userId'].unique()
+num_movies = ratings['movieId'].unique()
+#print(np.sort(num_movies), "\n")
 
-num_users = ratings_df['userId'].unique()
-num_movies = ratings_df['movieId'].unique()
-print(num_users.shape, num_movies.shape, "\n")
-print(np.sort(num_movies))
-
-
-
-#print(num_users)
-#print(num_movies)
-
-# Create the NumPy matrix
-rating_matrix = np.zeros((num_users, num_movies))
-
-# Fill the matrix with ratings
-for row in ratings_df.itertuples():
-    rating_matrix[row.userId - 1, row.movieId - 1] = row.rating
-
-#user = 391
-#movie = 2275
-
-#print(rating_matrix)
-#print(rating_matrix[user-1,movie-1])
-
-#exit(1)
+user_id = 4
+movie_id = 32
+user_index = pivot_table.index.get_loc(user_id)
+movie_index = pivot_table.columns.get_loc(movie_id)
+#print(rating_matrix[user_index, movie_index], "\n")
 
 # Example matrix
 R = np.array([
@@ -54,21 +37,22 @@ R = np.array([
 
 # Assigning it to the real matrix instead of example matrix
 R = rating_matrix
-print(R, "\n")
+print(R)
+print(R.shape, "\n")
 
 num_users, num_items = R.shape
 num_iters = 50 
 lamb = 0.1 # Used for overfitting
-num_features = 20 
+num_features = 100 
 
 U = np.random.rand(num_users, num_features)
 V = np.random.rand(num_items, num_features)
 
 # Alternating Least Squares (ALS) 
-def update_U(R, U, V): # U_i = (SUM V_j * V_j^T + lambda * I)^-1 * (SUM R_ij * V_j)
+def update_U(R, U, V): # U_i = (V_j * V_j^T + lambda * I)^-1 * R_ij * V_j^T
     for u in range(num_users):
         # Get indices of items user u has rated
-        rated_items = R[u, :] > 0
+        rated_items = R[u, :] >= 0.5
         V_rated = V[rated_items]
         R_u = R[u, rated_items] # Original matrix without the 0s
 
@@ -77,10 +61,10 @@ def update_U(R, U, V): # U_i = (SUM V_j * V_j^T + lambda * I)^-1 * (SUM R_ij * V
         b = V_rated.T @ R_u
         U[u] = np.linalg.solve(A, b) # Instead of inverse of matrix (^-1), we use Ax = b linear algorithm (faster and more accurate)
 
-def update_V(R, U, V): # V_j = (SUM U_i * U_i^T + lambda * I)^-1 * (SUM R_ij * U_i)
+def update_V(R, U, V): # V_j = (U_i * U_i^T + lambda * I)^-1 * R_ij * U_i^T
     for i in range(num_items):
         # Get indices of users who rated item i
-        rated_by = R[:, i] > 0
+        rated_by = R[:, i] >= 0.5
         U_rated = U[rated_by]
         R_i = R[rated_by, i]
 
@@ -106,6 +90,7 @@ def als():
 als()
 predicted_R = U @ V.T
 print(f"\n{np.round(predicted_R, 2)}")
+print(predicted_R.shape)
 
 output_file = "output.txt"
 #np.savetxt(filepath + output_file, np.round(predicted_R, 2), fmt="%.2f", delimiter=",")
