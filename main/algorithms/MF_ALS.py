41c560e7e3ef73aa49b06a1b6ae1f3bcea4aecba
IO32NQ <113096079+IO32NQ@users.noreply.github.com>
2025-05-01 15:45:47 +0200

    .

--- main/algorithms/MF_ALS.py
+++ main/algorithms/MF_ALS.py
@@ -106,19 +106,21 @@ def hyperparameter_tuning(R, num_iters, lamb, num_features, num_users, num_items
                 print("Best RMSE:", best_rmse)
     return best_params
 
-def hyperparameter_tuning_random(R, num_iters, lamb, num_features, num_users, num_items):
+def hyperparameter_tuning_random(R, num_users, num_items):
     # Define search space
-    latent_feature_choices = [10, 20, 30, 40, 50, 60, 70, 80]
+    latent_feature_choices = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     iteration_choices = [5, 10, 20, 30, 40, 50]
     lambda_choices = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]
 
     # How many random combinations to try
-    num_trials = 30
+    max_trials = 60
+    early_stopping_rounds = 10
+    no_improve_count = 0
 
     best_rmse = float('inf')
     best_params = None
 
-    for trial  in range(num_trials):
+    for trial in range(max_trials):
         # Randomly sample parameters
         rank = random.choice(latent_feature_choices)
         num_iter = random.choice(iteration_choices)
@@ -129,11 +131,20 @@ def hyperparameter_tuning_random(R, num_iters, lamb, num_features, num_users, nu
 
         pred = predict(U, V)
         rmse = compute_mask_rmse(val_matrix, pred, val_mask)
-        print(f"Trial = {num_trials+1}, Validation RMSE = {rmse:.4f}")
+        print(f"Trial = {trial+1}, Validation RMSE = {rmse:.4f}")
 
         if rmse < best_rmse:
             best_rmse = rmse
             best_params = (rank, reg, num_iter)
+            no_improve_count = 0
+        else:
+            no_improve_count += 1
+            print(f"No improvement count: {no_improve_count}")
+
+        # Early stopping
+        if no_improve_count >= early_stopping_rounds:
+            print(f"\nEarly stopping after {trial+1} number of trials with no improvement for {early_stopping_rounds} rounds.")
+            break
 
         print("Best hyperparameters:", best_params)
         print(f"Best RMSE: {best_rmse:.4f}" )
@@ -183,7 +194,8 @@ num_iters = [10, 50]
 num_features = [20, 50, 100]
 lamb = [0.01, 0.1]
 #rank, reg, num_iter = hyperparameter_tuning(val_matrix, num_iters, lamb, num_features, num_users, num_items)
-rank, reg, num_iter = (50, 0.01, 50)
+#rank, reg, num_iter = hyperparameter_tuning_random(val_matrix, num_users, num_items)
+rank, reg, num_iter = (80, 0.001, 50)
 print(f"Rank = {rank}, Reg = {reg}, Num_iter = {num_iter}")
 
 # Predict
